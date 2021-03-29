979e71964bcd1896f2fa9053dac76f54d2414739
hatoo <hato2000@gmail.com>
2021-03-30 00:07:21 +0900

    Simple token verifier

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use alcoholic_jwt::{token_kid, validate, Validation, JWKS};
+use anyhow::Context;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let authority = "https://hatoo.auth0.com/";
+    let jwks = reqwest::get(format!("{}.well-known/jwks.json", authority))
+        .await?
+        .json::<JWKS>()
+        .await?;
+
+    let validations = vec![
+        Validation::Issuer(authority.to_string()),
+        Validation::SubjectPresent,
+    ];
+
+    let token = std::env::var("Token")?;
+
+    let kid = token_kid(&token)
+        .ok()
+        .context("token_kid")?
+        .context("token_kid")?;
+
+    let jwk = jwks.find(&kid).context("find jwk")?;
+
+    let res = validate(token.as_str(), jwk, validations)
+        .ok()
+        .context("validate")?;
+
+    dbg!(res.claims);
+
+    Ok(())
 }
